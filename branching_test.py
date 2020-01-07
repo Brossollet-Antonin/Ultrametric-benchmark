70ba7174c5ccf534b9363a6f0fef459ef63967fa
Antonin Brossollet <antonin.brossollet@ens.fr>
2020-01-07 15:58:08 +0100

    This is a test commit!

---
+++ branching_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 15:42:31 2020
+
+@author: Antonin
+"""
+
+#Ceci est un document de test pour comprendre le branching avec git
+
+def f(x):
+    print('hello world' + str(x))
+    
