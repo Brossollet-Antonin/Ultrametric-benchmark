e5746f225f6bd50d0e0e6d53cc19356c97d53aca
slebastard <lebastardsimon@gmail.com>
2020-01-09 20:12:32 +0100

    Adding simple scripts to run MNIST and CIFAR10 jobs on cluster

---
+++ cluster_job/MNIST/individualjobMNIST.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# To run each individual job inside the loop
+
+#SBATCH --account=theory # The account name for the job.
+#SBATCH --job-name="MNIST $minibatchsize $blocksize" # The job name.
+#SBATCH -c 1 # The number of cpu cores to use.
+#SBATCH --time=8:00:00 # The time the job will take to run.
+#SBATCH --mem-per-cpu=4gb # The memory the job will use per cpu core.
+ 
+module load anaconda
+ 
+#Command to execute Python program
+python ../main.py --dataset MNIST --nnarch ResNet --resnettype 50 --minibatch ${1} --blocksz ${2} -T ${3} 
