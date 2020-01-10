34a970fbcaa7add097d84fcd33529663453ba519
Antonin Brossollet <antonin.brossollet@ens.fr>
2020-01-10 17:31:11 +0100

    Fixed test_resnet()

--- neuralnet.py
+++ neuralnet.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch.autograd import Variable 
+from torch.autograd import Variable
 
 
 # 2 hidden layers MLP with 256 ReLU units in each layers (similar to Chaudhry et al. (2019))
@@ -109,7 +109,7 @@ class ResNet(nn.Module):
 
 def resnetN(data_origin, type=50, num_classes=100):
     input_channel = 1 if data_origin=='MNIST' else 3
-    
+
     if type == 18:
         return ResNet(block=BasicBlock, num_blocks=[2,2,2,2], num_classes=num_classes, input_channel=input_channel)
     elif type == 34:
@@ -137,7 +137,7 @@ def ResNet152(num_classes=100):
     return ResNet(Bottleneck, [3,8,36,3], num_classes)
 
 def test_resnet():
-    net = ResNet50()
+    net = ResNetN(data_origin='CIFAR100', type=50, num_classes=100)
     y = net(Variable(torch.randn(1,3,32,32)))
     print(y.size())
 
@@ -151,32 +151,32 @@ class Net_FCRU(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size,hidden_size)
         self.fc3 = nn.Linear(hidden_size, output_size)
-        
+
     def forward(self, x):
         x = x.view(-1, input_size)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-    
+
     def layer2(self, x):
         x = x.view(-1, input_size)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-    
+
     def layer1(self, x):
         x = x.view(-1, input_size)
         x = self.fc1(x)
         return x
-    
+
     def layer3(self, x):
         x = x.view(-1, input_size)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return x   
-    
+        return x
+
     def layer4(self, x):
         x = x.view(-1, input_size)
         x = F.relu(self.fc1(x))
@@ -184,8 +184,8 @@ class Net_FCRU(nn.Module):
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
         return x
-    
-    
+
+
 # 2 hidden layers CNN
 
 class Net_CNN(nn.Module):
@@ -198,7 +198,7 @@ class Net_CNN(nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84,100) if dataset=='CIFAR100' else nn.Linear(84,10)
         self.dataset = dataset
-    
+
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
@@ -207,7 +207,7 @@ class Net_CNN(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-    
+
 hidden_size = 100
 
 class Net_FCL(nn.Module):
@@ -215,22 +215,19 @@ class Net_FCL(nn.Module):
         super(Net_FCL, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size,output_size)
-        
+
     def forward(self, x):
         x = x.view(-1, input_size)
         x = self.fc1(x)
         x = self.fc2(x)
         return x
-    
-    
+
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-# net = Net_CNN('MNIST')        
+# net = Net_CNN('MNIST')
 # nbr_para = count_parameters(net)
 # print(nbr_para)
-    
-
-
