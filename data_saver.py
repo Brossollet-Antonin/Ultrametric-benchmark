700439a6419d565219838050b89c3e2546435aa3
slebastard <lebastardsimon@gmail.com>
2020-01-22 21:06:52 +0100

    Minor correction with saving pickle files

--- data_saver.py
+++ data_saver.py
@@ -32,7 +32,7 @@ def save_results(rs, save_path):
 
     outfile = open(save_path + "/train_data_orig.pickle", 'wb')
     pickle.dump(rs.train_data_orig, outfile)
-    outfile.close
+    outfile.close()
 
     outfile = open(save_path + "/train_labels_orig.pickle", 'wb')
     pickle.dump(rs.train_labels_orig, outfile)
@@ -41,7 +41,69 @@ def save_results(rs, save_path):
 
     outfile = open(save_path + "/train_data_shfl.pickle", 'wb')
     pickle.dump(rs.train_data_shfl, outfile)
-    outfile.close
+    outfile.close()
+
+    outfile = open(save_path + "/train_labels_shfl.pickle", 'wb')
+    pickle.dump(rs.train_labels_shfl, outfile)
+    outfile.close()
+
+    outfile = open(save_path + "/distribution_train.pickle", 'wb')
+    pickle.dump(rs.classes_count, outfile)
+    outfile.close()
+
+    outfile = open(save_path+"/parameters.pickle", 'wb')
+    pickle.dump(rs.parameters, outfile)
+    outfile.close()
+
+    np.save(save_path + "/evaluation_original", rs.eval_orig)
+    np.save(save_path + "/evaluation_shuffled", rs.eval_shfl)
+
+    np.save(save_path+'/var_original_classes_prediction', rs.classes_pred_orig)
+    np.save(save_path+'/var_shuffle_classes_prediction', rs.classes_pred_shfl)
+    np.save(save_path+'/var_original_accuracy', rs.acc_orig)
+    np.save(save_path+'/var_shuffle_accuracy', rs.acc_shfl)
+
+    # np.save(save_path+'/autocorr_original', rs.atc_orig)
+    # np.save(save_path+'/autocorr_shuffle', rs.atc_shfl)
+
+    print('Saved all results to {0:s} subfolders'.format(save_path))
+
+
+def chunk_and_save(arr, max_size):
+    return
+
+
+def save_results_by_chunks(rs, save_path):
+    """
+    Save the results 
+
+    Parameters
+    ----------
+    rs : class ResultSet
+        Contains the different results to be saved
+    save_path : str
+        Path where to save the data.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    os.makedirs(save_path)
+
+    outfile = open(save_path + "/train_data_orig.pickle", 'wb')
+    pickle.dump(rs.train_data_orig, outfile)
+    outfile.close()
+
+    outfile = open(save_path + "/train_labels_orig.pickle", 'wb')
+    pickle.dump(rs.train_labels_orig, outfile)
+    outfile.close()
+
+
+    outfile = open(save_path + "/train_data_shfl.pickle", 'wb')
+    pickle.dump(rs.train_data_shfl, outfile)
+    outfile.close()
 
     outfile = open(save_path + "/train_labels_shfl.pickle", 'wb')
     pickle.dump(rs.train_labels_shfl, outfile)
