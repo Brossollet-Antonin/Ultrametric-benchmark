4359ca1b5f78e4e71ce701e6a46e220cc8b7030b
Antonin Brossollet <antonin.brossollet@ens.fr>
2020-04-20 13:10:56 +0200

    Update cluster job MNIST

--- cluster_job/MNIST/individualjobMNIST.sh
+++ cluster_job/MNIST/individualjobMNIST.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=theory # The account name for the job.
 #SBATCH --job-name="MNIST_UMRB" # The job name.
 #SBATCH -c 2 # The number of cpu cores to use.
-#SBATCH --time=35:00:00 # The time the job will take to run.
+#SBATCH --time=40:00:00 # The time the job will take to run.
 #SBATCH --mem-per-cpu=8gb # The memory the job will use per cpu core.
 #SBATCH --mail-type=START,END,FAIL,TIME_LIMIT_80                  # Mail notification of the events concerning the job : start time, end time,…
 #SBATCH --mail-user=antoninbrossollet@gmail.com
@@ -15,5 +15,5 @@ module load anaconda
 
 #Command to execute Python program
 python3 /rigel/theory/users/ab4877/Ultrametric-benchmark/main.py --dataset MNIST --nnarch FCL --hidden_sizes 256 --gpu \
---seqlength 1000000 --split_length 1000 --nbrtest 300 \
+--seqlength 1000000 --split_length 1000 --nbrtest 150 \
 -T ${1} --seqtype ${2} --blocksz ${3} ${4} ${5} ${6} ${7} ${8}
