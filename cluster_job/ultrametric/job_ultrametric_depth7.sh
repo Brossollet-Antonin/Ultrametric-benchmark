608eb9931fc30f782ee41c2269a716e5c77edd05
Simon Lebastard <sl4744@holmes.cm.cluster>
2020-05-09 16:44:38 -0400

    Reworked cluster jobs for ultrametric

--- cluster_job/ultrametric/job_ultrametric_depth7.sh
+++ cluster_job/ultrametric/job_ultrametric_depth7.sh
@@ -5,24 +5,28 @@
 tree_depth="7"
 hidden_size="50"
 minibatcheslist="10"
-blocksizearr=(1 82 164 328 1312 5248 20992)
+blocksizearr=(1 82 164 328 1312 5248 20992 41984 83968)
 temperaturelist="0.6"
-seqtypelist="ultrametric random_blocks2"
-shuffle_labels="1 0"
+flip_rates="0.04 0.07 0.1 0.13"
+seqtypelist="random_blocks2 ultrametric"
+shuffle_labels="0 1"
 split_length="328"
 
 for temperature in $temperaturelist
 do
-  for seqtype in $seqtypelist
+  for flip_rate in $flip_rates
   do
-  	for sl in $shuffle_labels
-  	do
-	  for value in {1..5}
-	  do
-	    sbatch individualjob_ultrametric.sh ${tree_depth} ${hidden_size} ${split_length} ${temperature} ${seqtype} ${sl} ${blocksizearr[*]} 
-	    sleep 1
-	  done
+    for seqtype in $seqtypelist
+    do
+      for sl in $shuffle_labels
+      do
+        for value in {1..10}
+	do
+	  sbatch individualjob_ultrametric.sh ${tree_depth} ${hidden_size} ${split_length} ${temperature} ${seqtype} ${sl} ${flip_rate} ${blocksizearr[*]} 
+	  sleep 1
 	done
+      done
+    done
   done
 done
 
