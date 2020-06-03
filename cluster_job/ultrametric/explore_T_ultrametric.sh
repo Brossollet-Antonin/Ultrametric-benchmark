c017a7e6063193fd76503c5ebbd50de8b88ff4d2
Simon Lebastard <lebastardsimon@gmail.com>
2020-06-03 14:41:20 +0200

    Updating temp exploration sbatch script

--- cluster_job/ultrametric/explore_T_ultrametric.sh
+++ cluster_job/ultrametric/explore_T_ultrametric.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
+#
+# Script to execute parameter exploration
 
-#SBATCH --job-name=explore_T_ultrametric # Job Name
-#SBATCH --nodes=2                        # Number of Nodes : 2
-#SBATCH --cpus-per-task=1                # 1 CPU allocation per Task
-#SBATCH --mem-per-cpu=8g
-
-#SBATCH --mail-type=ALL                  # Mail notification of the events concerning the job : start time, end time,…
-#SBATCH --mail-user=lebastardsimon@gmail.com
-
-tree_depth=5
-T_str=$1
-hidden_width=20
-nruns=4
-
+tree_depth=$1
+hidden_size=$2
+flip_rate=$3
+T_str=$4
 IFS=';' read -r -a T_list <<< "$T_str"
 
-for T in "${T_list[@]}"
+minibatcheslist="10"
+blocksizearr=(1)
+seqtypelist="ultrametric"
+shuffle_labels="1"
+split_length="328"
+
+for temperature in "${T_list[@]}"
 do
-	srun -n1 -N1 "python3 main.py -v --dataset artificial --data_tree_depth ${tree_depth} --data_seq_size 200 --seqlength 200000 --seqtype ultrametric --nbrtest 200 --nnarchi FCL --hidden_sizes ${hidden_width} -T ${T} " &
+  for seqtype in $seqtypelist
+  do
+    for sl in $shuffle_labels
+    do
+      for value in {1..3}
+	  do
+	    sbatch individualjob_ultrametric.sh ${tree_depth} ${hidden_size} ${split_length} ${temperature} ${seqtype} ${sl} ${flip_rate} ${blocksizearr[*]} 
+	    sleep 1
+	  done
+    done
+  done
 done
 
-wait
+	#End of script
---
+++ matlab/compute_autocorr.m
@@ -0,0 +1,273 @@
+function [ output_args ] = compute_autocorr( dataset_name, nnarchi, um_battery, rb2_battery )
+%COMPUTE_AUTOCORR Summary of this function goes here
+%   Detailed explanation goes here
+maxh=10000;
+
+project_root='~/ultrametric_benchmark/Ultrametric-benchmark';
+data_root=fullfile(project_root,'Results/1toM',dataset_name,nnarchi);
+um_root=fullfile(data_root,um_battery);
+rb2_root=fullfile(data_root,rb2_battery);
+
+% Getting parameter files
+um_params = jsondecode(fileread(fullfile(um_root, 'parameters.json')));
+rb2_params = jsondecode(fileread(fullfile(rb2_root, 'parameters.json')));
+
+% Checking consistency
+assert(um_params.TreeDepth==rb2_params.TreeDepth);
+assert(um_params.TreeBranching==rb2_params.TreeBranching);
+
+command=um_params.OriginalCommand;
+block_sizes_raw=split(command,"'--blocksz',");
+block_sizes_raw=block_sizes_raw(2,1);
+block_sizes = regexp(block_sizes_raw,'\d*','Match');
+block_sizes = [block_sizes{:}];
+tmp_sz = numel(block_sizes);
+tmp_blsz = NaN(tmp_sz,1);
+for k = 1:tmp_sz
+  tmp_blsz(k,1) = str2num(block_sizes{k});
+end
+block_sizes = tmp_blsz;
+
+% Reading block sizes, tree size, etc
+tree_levels=um_params.TreeDepth;
+branching=um_params.TreeBranching;
+tree_l=branching^tree_levels;
+
+% Initializing objects
+sequences.ultra.orig = int16.empty;
+sequences.ultra.shfl = containers.Map;
+sequences.rb.orig = int16.empty;
+sequences.rb.shfl = containers.Map;
+
+for block_sz_id = 1:length(block_sizes)
+    block_sz = int2str(block_sizes(block_sz_id));
+    sequences.ultra.shfl(block_sz) = int16.empty;
+    sequences.rb.shfl(block_sz) = int16.empty;
+end
+
+% Loading ultrametric sequences data
+um_simusets = dir(um_root);
+dirFlags = [um_simusets.isdir];
+um_simusets = um_simusets(dirFlags);
+
+for um_simuset_id = 1:length(um_simusets)
+    um_simuset = um_simusets(um_simuset_id).name;
+    matfiles_root = fullfile(um_root, um_simuset, 'matlab');
+    matfiles = dir(matfiles_root);
+    ndirFlags = ~[matfiles.isdir];
+    matfiles = matfiles(ndirFlags);
+    for matfile_id = 1:length(matfiles)
+        matfilename = matfiles(matfile_id).name;
+        new_seq = load(fullfile(um_root, um_simuset, 'matlab', matfilename));
+        new_seq = new_seq.sequence;
+        new_seq = new_seq(1,1:300000);
+        for block_sz_id = 1:length(block_sizes)
+            block_sz = int2str(block_sizes(block_sz_id));
+            if(contains(matfilename, strcat('_shfl_',block_sz,'.mat')))
+                sequences.ultra.shfl(block_sz) = [sequences.ultra.shfl(block_sz);new_seq];
+                break;
+            end
+        end
+
+        if(contains(matfilename, strcat('_orig.mat')))
+            sequences.ultra.orig = [sequences.ultra.orig;new_seq];
+        end
+    end
+end
+
+% Loading random block sequences data
+rb2_simusets = dir(rb2_root);
+dirFlags = [rb2_simusets.isdir];
+rb2_simusets = rb2_simusets(dirFlags);
+
+for rb2_simuset_id = 1:length(rb2_simusets)
+    rb2_simuset = rb2_simusets(rb2_simuset_id).name;
+    matfiles_root = fullfile(rb2_root, rb2_simuset, 'matlab');
+    matfiles = dir(matfiles_root);
+    ndirFlags = ~[matfiles.isdir];
+    matfiles = matfiles(ndirFlags);
+    for matfile_id = 1:length(matfiles)
+        matfilename = matfiles(matfile_id).name;
+        new_seq = load(fullfile(rb2_root, rb2_simuset, 'matlab', matfilename));
+        new_seq = new_seq.sequence;
+        new_seq = new_seq(1,1:300000);
+        for block_sz_id = 1:length(block_sizes)
+            block_sz = int2str(block_sizes(block_sz_id));
+            if(contains(matfilename, strcat('_shfl_',block_sz,'.mat')))
+                sequences.rb.shfl(block_sz) = [sequences.rb.shfl(block_sz);new_seq];
+                break;
+            end
+        end
+
+        if(contains(matfilename, strcat('_orig.mat')))
+            sequences.rb.orig = [sequences.rb.orig;new_seq];
+        end
+    end
+end
+
+% compute P_0 for ultrametric sequences
+
+hlocs_stat_ultra=zeros(length(block_sizes)+1,maxh-1);
+
+disp('Ultrametric - Computing P0 for original sequences...')
+for seq_id = 1:size(sequences.ultra.orig,1)
+    lbl_seq = sequences.ultra.orig(seq_id,:);
+    for i=1:tree_l
+        locs=find(lbl_seq==i); %find all occurrences of i 
+        nd=1;
+        for j=1:size(locs,2)
+            for k=(j+1):size(locs,2)
+                locsd(nd)=locs(k)-locs(j);
+                if(locsd(nd)>maxh)
+                    break;
+                end
+                nd=nd+1;
+            end
+        end
+
+        if(nd>1)
+            edges=1:maxh;
+            hlocs= histcounts(locsd(1:(nd-1)),edges,'Normalization','probability'); % compute the histogram of the lags
+            hlocs_stat_ultra(1,:)=hlocs_stat_ultra(1,:)+hlocs./tree_l;
+        end
+    end
+end
+
+for block_sz_id = 1:length(block_sizes)
+    block_sz = int2str(block_sizes(block_sz_id));
+    fprintf('Ultrametric - Computing P0 for shuffled sequences with block size %s...\n', block_sz)
+    seq_list = sequences.ultra.shfl(block_sz);
+    for seq_id = 1:size(sequences.ultra.shfl(block_sz),1)
+        lbl_seq = seq_list(seq_id,:);
+        for i=1:tree_l
+            locs=find(lbl_seq==i); %find all occurrences of i
+            nd=1;
+            for j=1:size(locs,2)
+                for k=(j+1):size(locs,2)
+                    locsd(nd)=locs(k)-locs(j);
+                    if(locsd(nd)>maxh)
+                        break;
+                    end
+                    nd=nd+1;
+                end
+            end
+
+            if(nd>1)
+            edges=1:maxh;
+            hlocs= histcounts(locsd(1:(nd-1)),edges,'Normalization','probability'); % compute the histogram of the lags
+            hlocs_stat_ultra(1+block_sz_id,:)=hlocs_stat_ultra(1+block_sz_id,:)+hlocs./tree_l;
+            end
+        end
+    end
+end
+
+
+% compute P_0 for random_blocks sequences
+
+hlocs_stat_rb=zeros(length(block_sizes)+1,maxh-1);
+
+disp('Random blocks - Computing P0 for original sequences...')
+for seq_id = 1:size(sequences.rb.orig,1)
+    lbl_seq = sequences.rb.orig(seq_id,:);
+    for i=1:tree_l
+        locs=find(lbl_seq==i); %find all occurrences of i
+        nd=1;
+        for j=1:size(locs,2)
+            for k=(j+1):size(locs,2)
+                locsd(nd)=locs(k)-locs(j);
+                if(locsd(nd)>maxh)
+                    break;
+                end
+                nd=nd+1;
+            end
+        end
+
+        if(nd>1)
+            edges=1:maxh;
+            hlocs= histcounts(locsd(1:(nd-1)),edges,'Normalization','probability'); % compute the histogram of the lags
+            hlocs_stat_rb(1,:)=hlocs_stat_rb(1,:)+hlocs./tree_l;
+        end
+    end
+end
+
+for block_sz_id = 1:length(block_sizes)
+    block_sz = int2str(block_sizes(block_sz_id));
+    fprintf('Random blocks - Computing P0 for shuffled sequences with block size %s...\n', block_sz)
+    seq_list = sequences.rb.shfl(block_sz);
+    for seq_id = 1:size(sequences.rb.shfl(block_sz),1)
+        lbl_seq = seq_list(seq_id,:);
+        for i=1:tree_l
+            locs=find(lbl_seq==i); %find all occurrences of i
+            nd=1;
+            for j=1:size(locs,2)
+                for k=(j+1):size(locs,2)
+                    locsd(nd)=locs(k)-locs(j);
+                    if(locsd(nd)>maxh)
+                        break;
+                    end
+                    nd=nd+1;
+                end
+            end
+
+            if(nd>1)
+            edges=1:maxh;
+            hlocs= histcounts(locsd(1:(nd-1)),edges,'Normalization','probability'); % compute the histogram of the lags
+            hlocs_stat_rb(1+block_sz_id,:)=hlocs_stat_rb(1+block_sz_id,:)+hlocs./tree_l;
+            end
+        end
+    end
+end
+
+
+% Plotting results
+figure(1);
+clf;
+hold on;
+
+ultra_cb = [1, 0, 0];
+ultra_ce = [230, 130, 130]/255;
+ultra_colors = [linspace(ultra_cb(1),ultra_ce(1),1+length(block_sizes))', linspace(ultra_cb(2),ultra_ce(2),1+length(block_sizes))', linspace(ultra_cb(3),ultra_ce(3),1+length(block_sizes))'];
+
+%legends = char.empty;
+legends = [];
+
+h=plot(hlocs_stat_ultra(1,2:2:end-1)./hlocs_stat_ultra(1,2),'Color',ultra_colors(1,:),'LineStyle','-');%,'DisplayName','Ultrametric - Original sequence');
+legends(1) = 'Ultrametric - Original sequence';
+set(h,'linewidth',2);
+
+for block_sz_id = 1:length(block_sizes)
+    fig_name = strcat('Ultrametric - Shuffled with block size ', int2str(block_sizes(block_sz_id)));
+    h=plot(hlocs_stat_ultra(1+block_sz_id,2:2:end-1)./hlocs_stat_ultra(1+block_sz_id,2),'Color',ultra_colors(1+block_sz_id,:),'LineStyle','--');%,'DisplayName',fig_name);
+    legends(1+block_sz_id) = fig_name;
+    set(h,'linewidth',2);
+end
+
+rb_cb = [0, 110, 0]/255;
+rb_ce = [145, 225, 145]/255;
+rb_colors = [linspace(rb_cb(1),rb_ce(1),1+length(block_sizes))', linspace(rb_cb(2),rb_ce(2),1+length(block_sizes))', linspace(rb_cb(3),rb_ce(3),1+length(block_sizes))'];
+
+h=plot(hlocs_stat_rb(1,2:2:end-1)./hlocs_stat_rb(1,2),'Color',rb_colors(1,:),'LineStyle','-');%,'DisplayName','Random blocks - Original sequence');
+legends(2+length(block_sizes)) = 'Random blocks - Original sequence';
+set(h,'linewidth',2);
+
+for block_sz_id = 1:length(block_sizes)
+    fig_name = strcat('Random blocks - Split size ', int2str(block_sizes(block_sz_id)));
+    h=plot(hlocs_stat_rb(1+block_sz_id,2:2:end-1)./hlocs_stat_rb(1+block_sz_id,2),'Color',rb_colors(1+block_sz_id,:),'LineStyle','--');%,'DisplayName',fig_name);
+    legends(2+length(block_sizes)+block_sz_id) = fig_name;
+    set(h,'linewidth',2);
+end
+
+% Figure configuration
+ax = gca;
+ax.FontSize = 16; 
+
+set(gca,'Xscale','log');
+set(gca,'Yscale','log');
+
+xlabel("Sequence space", 'FontSize', 18);
+ylabel("Autocorrelation", 'FontSize', 18);
+
+columnlegend(2, legends);
+
+end
+
