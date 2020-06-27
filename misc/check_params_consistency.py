6a26f6df83ea9de399036e1a89dd3e559232bfb8
Simon Lebastard <lebastardsimon@gmail.com>
2020-06-27 12:38:25 +0200

    Adding script to remove useless and heavy evaluation files

--- misc/check_params_consistency.py
+++ misc/check_params_consistency.py
@@ -10,6 +10,7 @@ paths = utils.get_project_paths()
 
 parser = argparse.ArgumentParser(os.path.join(paths['misc'], 'check_params_consistency.py'), description='Checking that each simulation set uses the same parameters, to ensure concsitency of the results')
 parser.add_argument('--simuset_root_dir', type=str, help="Root to the folder simulation set folders that will be checked (internally, independently from one another)")
+parser.add_argument('--remove_eval_files', action='store_true', default=False, help="If specified, will delete the (usually heavy) evaluation files that were created by default at simulation time before 06/27/20")
 
 args = parser.parse_args()
 
@@ -57,6 +58,20 @@ for simuset in os.listdir(args.simuset_root_dir):
         for param_name in params_that_should_match:
             if (param_name in simu_params.keys()) and (param_name in ref_params.keys()):
                 assert simu_params[param_name] == ref_params[param_name], "    {0:s} MISMATCH BETWEEN {1:s} AND {2:s}".format(param_name, simu_subdir, simu_ref_subdir)
+
+        if args.remove_eval_files:
+            # Original file
+            if os.path.exists(os.path.join(simuset_path, simu_subdir, 'evaluation_original.npy')):
+                os.remove(os.path.join(simuset_path, simu_subdir, 'evaluation_original.npy'))
+
+            # Shuffle files
+            shuffle_dirs = [shuffle_dir for shuffle_dir in os.listdir(os.path.join(simuset_path, simu_subdir)) if os.path.isdir(os.path.join(simuset_path, simu_subdir, shuffle_dir))]
+            for shuffle_dir in shuffle_dirs:
+                if 'shuffle_' not in shuffle_dir:
+                    continue
+                if os.path.exists(os.path.join(simuset_path, simu_subdir, shuffle_dir, 'evaluation_shuffled.npy')):
+                    os.remove(os.path.join(simuset_path, simu_subdir, shuffle_dir, 'evaluation_shuffled.npy'))
+            print("Removed evaluation files from simulation result folders")
 
     print("Simulation set {:s} is consistent! Producing a single parameters file for the simulation set...".format(simuset))
     simuset_params = {k: ref_params[k] for k in params_that_should_match if k in ref_params.keys()}
