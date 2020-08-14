0d9d51c5aeead2f0eddf9fae30a16e2494ae7df6
Antonin Brossollet <antonin.brossollet@ens.fr>
2020-08-14 12:21:12 +0200

    Correct nnarchi parameter typo

--- cluster_job/individual_simu.sh
+++ cluster_job/individual_simu.sh
@@ -9,7 +9,7 @@
 module load anaconda
 
 #Command to execute Python program
-python3 ${1} --dataset ${2} --data_tree_depth ${3} -T ${4} --nnarch {5} --hidden_sizes ${6} \
+python3 ${1} --dataset ${2} --data_tree_depth ${3} -T ${4} --nnarch ${5} --hidden_sizes ${6} \
 --optimizer ${7} --nonlin ${8} --lr ${9} --seqtype ${10} --seqlength ${11} --split_length ${12} \
 --nbrtest ${13} --data_flips_rate ${14} --shuffle_classes ${15} --blocksz ${16} --verbose 1 \
   
