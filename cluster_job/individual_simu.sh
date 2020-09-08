1000d1a657505a7ce3a0b685afa507fecd692bfd
Simon Lebastard <sl4744@watson.cm.cluster>
2020-09-08 06:49:52 -0400

    Minor correction to verbose feature

--- cluster_job/individual_simu.sh
+++ cluster_job/individual_simu.sh
@@ -28,4 +28,4 @@ echo "Verbose level: ${18}"
 #Command to execute Python program
 python3 ${1} --dataset ${2} --data_tree_depth ${3} -T ${4} --nnarch ${5} --hidden_sizes ${6} \
 --optimizer ${7} --nonlin ${8} --lr ${9} --seqtype ${10} --seqlength ${11} --split_length ${12} \
---nbrtest ${13} --data_flips_rate ${14} --shuffle_classes ${15} --blocksz ${16} --use_orig ${17:-""} --verbose {18} \
+--nbrtest ${13} --data_flips_rate ${14} --shuffle_classes ${15} --blocksz ${16} --use_orig ${17:-""} --verbose ${18} \
